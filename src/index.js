dc58c4af22b13e6f7d0ecca6ecb33b35deba93e5
Aris Kostopoulos <arisbkostopoulos@gmail.com>
2021-04-04 19:25:52 +0300

    entry

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'; //need to import
+import ReactDom from 'react-dom'; // normally only here
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// stateless functional component
+// always return JSX
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+    return (
+        <section className='booklist'>
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
+        </section>
+    );
+};
+
+const Book = () => {
+    return <article className='book'>
+        <Image/>
+        <Title/>
+        <Author/>
+    </article>
+}
+const Image = () => {
+    return <img src="https://m.media-amazon.com/images/I/71CedQlX1fL.jpg" alt="" />
+}
+const Title = () => {
+    return <h1>The Code Breaker</h1>
+}
+const Author = () => {
+    return <h3>WALTER ISAACSON</h3>
+}
+
+
+ReactDom.render(<BookList/>, document.getElementById('root'));
