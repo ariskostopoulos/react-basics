9db06e0dd8ae3b04c560b1d6246b0917327d5b8c
Aris Kostopoulos <arisbkostopoulos@gmail.com>
2021-04-05 16:49:05 +0300

    still learnign basics

--- src/index.js
+++ src/index.js
@@ -4,34 +4,42 @@ import './index.css'
 
 // stateless functional component
 // always return JSX
+const books = [
+    {
+        title: 'The Code Breaker',
+        author: 'WALTER ISAACSON',
+        img: 'https://m.media-amazon.com/images/I/71CedQlX1fL.jpg'
+    },
+    {
+        title: 'The Code Breaker 2',
+        author: 'WALTER ISAACSON 2',
+        img: 'https://m.media-amazon.com/images/I/71CedQlX1fL.jpg'
+    }
+]
 
 function BookList() {
     return (
         <section className='booklist'>
-            <Book/>
-            <Book/>
-            <Book/>
-            <Book/>
+            {books.map((book) => {
+                const {img, title, author} = book;
+                return (
+                    <Book book = {book}/>
+                )
+            })}
         </section>
     );
-};
-
-const Book = () => {
-    return <article className='book'>
-        <Image/>
-        <Title/>
-        <Author/>
-    </article>
-}
-const Image = () => {
-    return <img src="https://m.media-amazon.com/images/I/71CedQlX1fL.jpg" alt="" />
-}
-const Title = () => {
-    return <h1>The Code Breaker</h1>
-}
-const Author = () => {
-    return <h3>WALTER ISAACSON</h3>
 }
 
+const Book = (props) => {
+
+    return (
+        <article className='book'>
+            <img src={props.book.img} alt="" />
+            <h1>{props.book.title}</h1>
+            <h3>{props.book.author}</h3>
+
+        </article>
+    )
+}
 
 ReactDom.render(<BookList/>, document.getElementById('root'));
